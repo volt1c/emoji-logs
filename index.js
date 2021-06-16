586eb11382767ebfff3cedb80f8621be4ce98c88
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-06-16 10:17:04 +0200

    feat: extensive configuration new version

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const config = () => {
+const config = ({ info = '✅', error = '⛔️', warn = '⚡', debug = '🔍' }) => {
   const addPrefix = (consoleLog, emoji) => {
     const log = consoleLog
     return (msg) => log(`[${emoji}]> ${msg}`)
   }
-  console.info = addPrefix(console.info, `✅`)
-  console.error = addPrefix(console.error, `⛔️`)
-  console.warn = addPrefix(console.warn, `⚡`)
-  console.debug = addPrefix(console.debug, `🔍`)
+  console.info = addPrefix(console.info, info)
+  console.error = addPrefix(console.error, error)
+  console.warn = addPrefix(console.warn, warn)
+  console.debug = addPrefix(console.debug, debug)
 }
 
 module.exports = {
