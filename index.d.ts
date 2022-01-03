b509730e34ef121dd1f71806756ad13b83ce45d2
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-03 10:01:15 +0100

    feat: support typescript

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+export type ConfigOptions = {
+  info: string
+  error: string
+  warn: string
+  debug: string
+  formatter: (v: string) => string
+}
+
+/**
+ * Method to config `emoji-logs`
+ *
+ * @param {ConfigOptions} options
+ */
+export function config(options: ConfigOptions): void
